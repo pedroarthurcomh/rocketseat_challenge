db1feedbe850030a43f2a7d9f5905f720857ac99
Pedro Barros <am.pedrobarros@outlook.com>
2023-11-02 12:30:43 -0300

    feat: products list integrated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,13 +8,27 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products: any;
+  category_filter:number = 1;
 
   constructor(private router: Router, private products_service: ProductsService) {}
 
   ngOnInit(): void {
     this.products_service.get_all_products().subscribe((res: any) => {
-      console.log(res)
+      this.products = res
     })
+  }
+
+  price(initial_price: number){
+    var price = initial_price/100
+    return `R$ ${price.toLocaleString('pt-BR', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })}`
+  }
+
+  change_filter_category(value: number){
+    this.category_filter = value
   }
 
 }
