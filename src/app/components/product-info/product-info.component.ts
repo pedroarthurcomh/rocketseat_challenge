e8c8a2f24e1a3da3b8f2d093611657fa10056d9b
Pedro Barros <am.pedrobarros@outlook.com>
2023-11-02 12:31:21 -0300

    feat: product info page done

--- src/app/components/product-info/product-info.component.ts
+++ src/app/components/product-info/product-info.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product-info',
   templateUrl: './product-info.component.html',
   styleUrls: ['./product-info.component.css']
 })
-export class ProductInfoComponent {
+export class ProductInfoComponent implements OnInit {
+  product_id: any;
+  product: any;
 
+  constructor(private router: Router, private routee: ActivatedRoute, private product_service: ProductsService) {
+    this.product_id = this.routee.snapshot.params['id']
+  }
+
+  ngOnInit(): void {
+    this.product_service.get_specific_product(this.product_id).subscribe((res: any) => {
+      this.product = res
+    })
+  }
+
+  
+  add_to_cart(){
+    let cart_exist = localStorage.getItem('cart')
+
+    let old_cart = []
+    if(cart_exist){
+      old_cart = JSON.parse(cart_exist)
+    }
+    old_cart.push(this.product)
+    let new_cart = JSON.stringify(old_cart)
+    localStorage.setItem('cart', new_cart)
+
+    this.product_service.item_added$.emit(1)
+  }
+  
+  price(initial_price: number){
+    var price = initial_price/100
+    return `R$ ${price.toLocaleString('pt-BR', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })}`
+  }
 }
