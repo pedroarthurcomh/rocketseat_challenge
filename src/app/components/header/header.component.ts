08d4b54ac0baf1359aa722bfc5b65e9ca34b8003
Pedro Barros <am.pedrobarros@outlook.com>
2023-11-02 12:30:05 -0300

    feat: header logic done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  quantity: number = 0;
+
+  constructor(private product_service: ProductsService) {
+    this.product_service.item_added$.subscribe((value: any) => {
+      var cart_number: any = localStorage.getItem('cart')
+      this.quantity = JSON.parse(cart_number).length
+    })
+  }
+
+  ngOnInit(): void {
+    var cart_number: any = localStorage.getItem('cart')
+    this.quantity = JSON.parse(cart_number).length
+  }
 
 }
